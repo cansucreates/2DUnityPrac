e03e4581b3f6334ff45e5cf572a485e17570f1b0
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-03-16 23:47:35 +0300

    setup 2d project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = 0.0f;
+
+        if (Keyboard.current.leftArrowKey.isPressed) {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed) {
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+
+
+      Vector2 position = transform.position;
+      position.x = position.x + 0.1f * speed;;  
+      position.y = position.y + 0.1f * speed;
+      transform.position = position;
+    }
+}
