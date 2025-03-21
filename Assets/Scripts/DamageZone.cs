b54d2683ffab3e63a602469a754eef88311bc175
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-03-21 22:25:34 +0300

    Fixed damagezone

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    public int damageAmount;
     void OnTriggerStay2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
 
         if (controller != null)
         {
-            controller.ChangeHealth(-1);
+            controller.ChangeHealth(-damageAmount);
         }
     }
 }
