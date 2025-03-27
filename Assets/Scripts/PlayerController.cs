1f65ccf9a5796ce34f42fcfed435413e632e4b82
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-03-27 15:37:31 +0300

    Added NPC interaction raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction talkAction; // for npc talk
     public GameObject projectilePrefab;
     Animator animator;
     Vector2 moveDirection = new Vector2(1, 0); // when the char is still, both movex and move y will be 0, so
@@ -27,6 +28,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        talkAction.Enable();
         animator = GetComponent<Animator>();
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -61,10 +63,16 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // projectile shooting 
+        // projectile shooting
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
+        }
+
+        // raycast to interact with npc
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            FindFriend();
         }
     }
 
@@ -92,12 +100,32 @@ public class PlayerController : MonoBehaviour
     }
 
     // projectile shooting
-    void Launch() {
+    void Launch()
+    {
         // create a new projectile and set the direction and force
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        GameObject projectileObject = Instantiate(
+            projectilePrefab,
+            rigidbody2d.position + Vector2.up * 0.5f,
+            Quaternion.identity
+        );
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(moveDirection, 300);
         animator.SetTrigger("Launch");
+    }
 
+    // raycast to interact with npc
+    void FindFriend()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            rigidbody2d.position + Vector2.up * 0.2f,
+            moveDirection,
+            1.5f,
+            LayerMask.GetMask("NPC")
+        );
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+        }
     }
 }
